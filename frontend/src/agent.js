9dad066c754bb020378ac24c06a2bfd83e6a0f92
Scott Young <young.scottw@gmail.com>
2022-09-20 13:59:22 -0500

    Search by title

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -1,12 +1,12 @@
-import superagentPromise from "superagent-promise";
-import _superagent from "superagent";
+import superagentPromise from 'superagent-promise';
+import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
 const BACKEND_URL =
-  process.env.NODE_ENV !== "production"
+  process.env.NODE_ENV !== 'production'
     ? process.env.REACT_APP_BACKEND_URL
-    : "https://api.anythink.market";
+    : 'https://api.anythink.market';
 
 const API_ROOT = `${BACKEND_URL}/api`;
 
@@ -16,7 +16,7 @@ const responseBody = (res) => res.body;
 let token = null;
 const tokenPlugin = (req) => {
   if (token) {
-    req.set("authorization", `Token ${token}`);
+    req.set('authorization', `Token ${token}`);
   }
 };
 
@@ -38,22 +38,25 @@ const requests = {
 };
 
 const Auth = {
-  current: () => requests.get("/user"),
+  current: () => requests.get('/user'),
   login: (email, password) =>
-    requests.post("/users/login", { user: { email, password } }),
+    requests.post('/users/login', { user: { email, password } }),
   register: (username, email, password) =>
-    requests.post("/users", { user: { username, email, password } }),
-  save: (user) => requests.put("/user", { user }),
+    requests.post('/users', { user: { username, email, password } }),
+  save: (user) => requests.put('/user', { user }),
 };
 
 const Tags = {
-  getAll: () => requests.get("/tags"),
+  getAll: () => requests.get('/tags'),
 };
 
 const limit = (count, p) => `limit=${count}&offset=${p ? p * count : 0}`;
 const omitSlug = (item) => Object.assign({}, item, { slug: undefined });
 const Items = {
-  all: (page) => requests.get(`/items?${limit(1000, page)}`),
+  all: (page, title) =>
+    title !== undefined
+      ? requests.get(`/items?${limit(1000, page)}&title=${title}`)
+      : requests.get(`/items?${limit(1000, page)}`),
   bySeller: (seller, page) =>
     requests.get(`/items?seller=${encode(seller)}&${limit(500, page)}`),
   byTag: (tag, page) =>
@@ -62,12 +65,12 @@ const Items = {
   favorite: (slug) => requests.post(`/items/${slug}/favorite`),
   favoritedBy: (seller, page) =>
     requests.get(`/items?favorited=${encode(seller)}&${limit(500, page)}`),
-  feed: () => requests.get("/items/feed?limit=10&offset=0"),
+  feed: () => requests.get('/items/feed?limit=10&offset=0'),
   get: (slug) => requests.get(`/items/${slug}`),
   unfavorite: (slug) => requests.del(`/items/${slug}/favorite`),
   update: (item) =>
     requests.put(`/items/${item.slug}`, { item: omitSlug(item) }),
-  create: (item) => requests.post("/items", { item }),
+  create: (item) => requests.post('/items', { item }),
 };
 
 const Comments = {
