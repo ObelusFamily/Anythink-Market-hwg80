02b5fd2cc64d4874466a44eaf860c001582a6671
Scott Young <young.scottw@gmail.com>
2022-09-20 11:39:51 -0500

    Add placeholder image to ItemPreview

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
+import placeholder from "../imgs/placeholder.png";
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -34,12 +35,12 @@ const ItemPreview = (props) => {
       className="card bg-dark border-light p-3"
       style={{ borderRadius: "20px" }}
     >
-      <img
+      {item.image ? <img
         alt="item"
         src={item.image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
-      />
+      /> : <img alt="item" src={placeholder} className="card-img-top item-img" style={{ borderRadius: "20px" }} />}
       <div className="card-body">
         <Link to={`/item/${item.slug}`} className="text-white">
           <h3 className="card-title">{item.title}</h3>
