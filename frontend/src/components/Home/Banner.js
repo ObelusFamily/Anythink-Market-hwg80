9e370b681992de44ddda5e577c5ddb8109b63cc8
Scott Young <young.scottw@gmail.com>
2022-09-20 13:57:06 -0500

    Search for title

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,15 @@
-import React from "react";
-import logo from "../../imgs/logo.png";
+import React from 'react';
+import logo from '../../imgs/logo.png';
 
-const Banner = () => {
+const Banner = (props) => {
+  const { onSearch } = props;
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <input onChange={(e) => onSearch(e.target.value)} type="text" />
           <span> the cool stuff.</span>
         </div>
       </div>
--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,14 +1,15 @@
-import Banner from "./Banner";
-import MainView from "./MainView";
-import React from "react";
-import Tags from "./Tags";
-import agent from "../../agent";
-import { connect } from "react-redux";
+import Banner from './Banner';
+import MainView from './MainView';
+import React from 'react';
+import Tags from './Tags';
+import agent from '../../agent';
+import { connect } from 'react-redux';
 import {
   HOME_PAGE_LOADED,
   HOME_PAGE_UNLOADED,
   APPLY_TAG_FILTER,
-} from "../../constants/actionTypes";
+  SEARCH,
+} from '../../constants/actionTypes';
 
 const Promise = global.Promise;
 
@@ -23,12 +24,13 @@ const mapDispatchToProps = (dispatch) => ({
     dispatch({ type: APPLY_TAG_FILTER, tag, pager, payload }),
   onLoad: (tab, pager, payload) =>
     dispatch({ type: HOME_PAGE_LOADED, tab, pager, payload }),
+  onSearch: (items) => dispatch({ type: SEARCH, payload: { items } }),
   onUnload: () => dispatch({ type: HOME_PAGE_UNLOADED }),
 });
 
 class Home extends React.Component {
   componentWillMount() {
-    const tab = "all";
+    const tab = 'all';
     const itemsPromise = agent.Items.all;
 
     this.props.onLoad(
@@ -42,10 +44,20 @@ class Home extends React.Component {
     this.props.onUnload();
   }
 
+  async handleSearch(searchString) {
+    if (searchString.length > 2) {
+      const itemsResult = await agent.Items.all(undefined, searchString);
+      this.props.onSearch(itemsResult.items);
+    } else {
+      const itemsResult = await agent.Items.all();
+      this.props.onSearch(itemsResult.items);
+    }
+  }
+
   render() {
     return (
       <div className="home-page">
-        <Banner />
+        <Banner onSearch={(e) => this.handleSearch(e)} />
 
         <div className="container page">
           <Tags tags={this.props.tags} onClickTag={this.props.onClickTag} />
